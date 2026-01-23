9198c22e816ba930553c9a808622624ac75bf37c
rner <duhairillahred927@gmail.com>
2026-01-23 14:03:04 +0700

    first commit

---
+++ ListKel.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class ListKel {
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        String nama[] = { "Sulivan", "RIzky", "Rillah", "Ega", "Rasyid", "Masrian" };
+
+        System.out.println("Daftar List Kelompok 3: ");
+        for (String i : nama) {
+            System.out.println(i);
+        }
+    }
+}
